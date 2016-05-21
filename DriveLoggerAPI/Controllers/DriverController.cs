7c1b82608e042588e077f8275ff5647dd2fde0b3
Klas Flodqvist <klas@alskami.se>
2016-05-20 19:41:45 -0700

    Add api project

---
+++ DriveLoggerAPI/Controllers/DriverController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DriveLoggerAPI.Models;
+using Newtonsoft.Json;
+
+namespace DriveLoggerAPI.Controllers
+{
+
+    public class DriverController : ApiController
+    {
+        // GET: api/Driver
+        public IEnumerable<DriverModel> Get()
+        {
+            var driver1 = new DriverModel { id = 1, name = "Klas Floda", socialsec = "781201-1122" };
+            var driver2 = new DriverModel { id = 2, name = "Kanne Kivi", socialsec = "781001-1020" };
+            var dr1 = JsonConvert.SerializeObject(driver1);
+            var dr2 = JsonConvert.SerializeObject(driver2);
+            return new List<DriverModel>() { driver1, driver2 };
+            //return new string[] { dr1, dr2 };
+        }
+
+        // GET: api/Driver/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Driver
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Driver/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Driver/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DriveLoggerAPI/Models/DriverModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DriveLoggerAPI.Models
+{
+    public class DriverModel
+    {   
+        public int id { get; set; }
+        public string name { get; set; }
+        public string socialsec { get; set; }
+    }
+}
